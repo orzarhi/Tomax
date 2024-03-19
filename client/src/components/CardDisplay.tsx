c71bd03a43de9c868b1d0083653a220a3bfc7728
orzarhi <or.zarhi11@gmail.com>
2024-03-19 08:38:24 +0200

    Update .gitignore and CardDisplay component

--- client/src/components/CardDisplay.tsx
+++ client/src/components/CardDisplay.tsx
@@ -1,22 +1,23 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { NewsType } from "@/types/news";
+import { format } from "date-fns";
 import { Link } from "react-router-dom";
 
 interface CardDisplayProps {
     item: NewsType
 }
 
-export const CardDisplay = ({ item: { title, description, urlToImage, id } }: CardDisplayProps) => {
+export const CardDisplay = ({ item: { title, description, urlToImage, id, publishedAt, author } }: CardDisplayProps) => {
     return (
         <Card className="w-full max-w-sm mx-auto shadow-sm cursor-pointer hover:shadow-black/70 hover:shadow-sm">
             <Link to={`/details/${id}`}>
                 <div className="p-4">
                     <CardHeader className="pb-6">
                         <CardTitle>{title}</CardTitle>
-                        <CardDescription>A beautiful new design framework</CardDescription>
+                        <CardDescription>{author}</CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-4">
-                        <p className="text-sm text-gray-500 dark:text-gray-400">Published on March 17, 2024</p>
+                        <p className="text-sm text-gray-500 dark:text-gray-400">Published on {format(publishedAt, 'MMMM, dd yyyy')}</p>
                         <img
                             alt="Cover image"
                             className="object-cover overflow-hidden rounded-lg aspect-video"

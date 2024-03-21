b6c8a06ba180ad895e44a7d0bb5b4e02ab258b2b
orzarhi <or.zarhi11@gmail.com>
2024-03-21 10:24:50 +0200

    Add localhost URL to CORS origin

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL, 'http://127.0.0.1:3000'],
     credentials: true
 }));
 
